992dde89c8129a31c0a570e0f6bf0364e3280390
Merlinz <jamkapp556@gmail.com>
2023-02-28 14:49:28 +0700

    Update Navbar.js

--- Navbar.js
+++ Navbar.js
@@ -2,7 +2,9 @@ import './navbar.css';
 function Navbar(){
     return(
         <nav className='nav'>
-            <a className='site_title' style={{ color: '#f57f7f' }}>TESTTO</a>
+            <div className='logo_top' >
+                <img src={("/images/LOGO.png")} width={150}  height={20}></img>
+            </div>  
             <ul>
                 <li className='active'>
                     <a href='/information' className='menu_text' >เกี่ยวกับ</a>
@@ -10,20 +12,25 @@ function Navbar(){
                 <li className='active'>
                     <a href='/select_car' className='menu_text' >เลือกรถ</a>
                 </li>
-                <li>
+                <li className='active'>
                     <a href='=/register' className='menu_text'>วิธีสมัคร</a>
                 </li>
-                <li>
+                <li className='active'>
                     <a href='=/contact' className='menu_text'>ติดต่อ/สอบถาม</a>
                 </li>
-                <li>
+                <li className='active'>
                     <a href='=/Booking' className='menu_text'>ข้อมูลการจอง</a>
                 </li>
-                <li>
+                <li className='active'>
                     <a href='=/lessing' className='menu_text'>ข้อมูลสัญญาเช่าซื้อ</a>
                 </li>
+                <li className='active'>
+                    <a href='=/user' className='user_text'></a>
+                </li>
+                {/* <li className='active'>
+                    <a href='=/user' className='user_text' > User</a>
+                </li>  */}
             </ul>
-            
         </nav>
         
     );
