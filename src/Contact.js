f0514aa1b2c1aeda33d9458ed7df205a2d54e2d9
Merlinz <jamkapp556@gmail.com>
2023-03-03 21:11:53 +0700

    Update Contact.js

--- src/Contact.js
+++ src/Contact.js
@@ -1,20 +1,17 @@
 import './Contact.css';
-import Navbar_Footer from './navbar_Footer';
-import Navbar from './Navbar';
+
 
 function Contact() {
   return (
     <div className="Main">
         <header className='main_header'>
           <div className='contact_pic'>
-            <Navbar/>
             <div className='Banner'>
               <img src= {("/images/ติดต่อ.png")} width={1800}></img>
             </div>
             <div className='BG_Contact'>
               <img  src= {("/images/BG_contact.png")} width={1800}></img>
             </div>
-            <Navbar_Footer/>
           </div>
         </header>
     </div>
