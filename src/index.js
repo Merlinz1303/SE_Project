96b5a690bf8834d87765cd2e411177ee72c3df1d
Merlinz <jamkapp556@gmail.com>
2023-03-03 15:35:25 +0700

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Navbar from './Navbar';
+import Select_car from './select_car';
+import Navba_footer from './navbar_Footer';
+import Booking from './Booking_car';
+import Information from './Information';
+import Contact from './Contact';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Contact/>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
