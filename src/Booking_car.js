930cd2c8ada489537039d2a19f85aa17c069b9ec
Merlinz <jamkapp556@gmail.com>
2023-03-03 21:11:50 +0700

    Update Booking_car.js

--- src/Booking_car.js
+++ src/Booking_car.js
@@ -1,16 +1,11 @@
 import './Booking_car.css';
-import Navbar_Footer from './navbar_Footer';
-import Navbar from './Navbar';
 
 function Booking() {
     return (
         <div className= "Booking_main">
-            <Navbar/>
             <p className='text'>ข้อมูลการจอง</p>
             <div className='Footer2'>
-                <Navbar_Footer/>
             </div>
-            
         </div>
     
     );
